cf3a52f46ce48de9a8d4eca86a16f0702e5c254c
Putro <lafa16ac@student.cbs.dk>
2019-06-07 15:36:35 +0200

    Initial restructuring and creation of to-do list

--- sdk.js
+++ sdk.js
@@ -1,11 +1,59 @@
 $(document).ready(() => {
-	const debug = true;
 
+	/*
+	 * TODO: 
+	 * 1. Initial index.html with link to Guide.html and SexItUp.html on github pages
+	 * 2. Create possibility to upload CSV file: https://www.youtube.com/watch?v=ZZncFax8yNY
+	 * 	2.1. Ensure that participants variable is populated with the information and saved in localStorage or sessionStorage
+	 * 	2.2. Create button to read/write from/to localStorage/sessionStorage if necessary¨
+	 * 	2.3. Update .gitignore to not upload the real data .csv, but to maintain the fake.csv 
+	 * 3. Create a reset button to remove all information 
+	 * 4. Populate a table with the read information and have it accessible only through SexItUp page -- separate page? or modal?
+	 * 5. Refactor individual distribution modules to english and better code
+	 * 	5.1. Cabins
+	 * 	5.2. Busses
+	 * 	5.3. Final Cleaning
+	 * 	5.4. Chore 1 and 2 + Associated days
+	 * 		5.4.1. Ensure that it cannot distribute chores on the same days
+	 * 6. Style everything somewhat alright
+	 * 7. Ask people to break it with fake data csv import
+	 */
+
+	const debug = false;
+
+	/** DEPRECATED ONCE CSV FILES CAN BE UPLOADED
+	 * The JSON format that needs to be used:
+	 * Use the accompanying excel file to create necessary data
+	{
+            "Name": "",
+            "Sex": "",
+            "Hytte": "",
+            "Tjans1": "",
+            "Tjans2": "",
+            "Bus": "",
+            "Slutrengoering": "",
+            "Tjansdag": "",
+            "Tjans2dag": ""
+        },
+	 */
+	let	participants = [{
+            "Name": "",
+            "Sex": "",
+            "Hytte": "",
+            "Tjans1": "",
+            "Tjans2": "",
+            "Bus": "",
+            "Slutrengoering": "",
+            "Tjansdag": "",
+            "Tjans2dag": ""
+		},];
+	/** The names of the busses */
 	const Bus1 = "Fremtiden";
 	const Bus2 = "Fortiden";
+	/** First participant is allocated to bus number one by default */
+	let busNr = 1;
 
-
-	/** Det første tal er nuværende antal på den givne tjans/hytte, det andet tal er maks */
+	/* Det første tal er nuværende antal på den givne tjans/hytte, det andet tal er maks */
 	/** Kiosk opfyldning tjanser*/
 	let kioskOpfyldningLordag = [0, 4, "Kiosk Opfyldning", "Lørdag"];
 	let kioskOpfyldningSondag = [0, 4, "Kiosk Opfyldning", "Søndag"];
@@ -37,15 +85,18 @@ $(document).ready(() => {
 	let rengoringUdenforSondag = [0, 10, "Rengøring Yderområder", "Søndag"];
 	let rengoringUdenforMandag = [0, 10, "Rengøring Yderområder", "Mandag"];
 
-	let remainingTjanser = 4 + 4 + 4 + 7 + 7 + 7 + 6 + 6 + 6 + 6 + 6 + 10 + 10 + 10 + 10 + 10 + 10;
-
-	const listeAfTjanser = ['aftensmadOpvaskSøndag', 'aftensmadOpvaskMandag', 'kioskOpfyldningLørdag', 'kioskOpfyldningSøndag', 'kioskOpfyldningMandag', 'rengøringIndendørsSøndag', 'rengøringIndendørsMandag', 'morgenmadSøndag', 'morgenmadMandag', 'morgenmadTirsdag', 'opvaskFrokostSøndag', 'opvaskFrokostMandag', 'aftensmadTilberedningSøndag', 'aftensmadTilberedningMandag', 'rengøringUdenforSøndag', 'rengøringUdenforMandag'];
-	const listeAfSlutrengoring = ['slutRengøringKøkken', 'slutRengøringUdenfor', 'slutRengøringIndenfor'];
-
 	/** Slut rengøring */
 	const slutRengoringKokken = [0, 15, "Køkken"];
 	const slutRengoringUdenfor = [0, 35, "Udenfor"];
 	const slutRengoringIndenfor = [0, 35, "Indenfor"];
+
+	const listeAfTjanser = ['aftensmadOpvaskSøndag', 'aftensmadOpvaskMandag', 'kioskOpfyldningLørdag', 'kioskOpfyldningSøndag', 'kioskOpfyldningMandag', 'rengøringIndendørsSøndag', 'rengøringIndendørsMandag', 'morgenmadSøndag', 'morgenmadMandag', 'morgenmadTirsdag', 'opvaskFrokostSøndag', 'opvaskFrokostMandag', 'aftensmadTilberedningSøndag', 'aftensmadTilberedningMandag', 'rengøringUdenforSøndag', 'rengøringUdenforMandag'];
+	const listeAfSlutrengoring = ['slutRengøringKøkken', 'slutRengøringUdenfor', 'slutRengøringIndenfor'];
+	const antalTjanser = listeAfTjanser.length;
+	const antalSlutTjanser = listeAfSlutrengoring.length;
+
+	/** TODO: Automate remainingtjanser count to rely on listeAfTjanser */
+	let remainingTjanser = 4 + 4 + 4 + 7 + 7 + 7 + 6 + 6 + 6 + 6 + 6 + 10 + 10 + 10 + 10 + 10 + 10;
 
 	/** Hytter */
 	let HytteA1 = [0, 9, "Hytte A1"];
@@ -59,10 +110,7 @@ $(document).ready(() => {
 	const HyttePiger = [0, 20, "Hytte E"];
 	const listeAfDrengeHytter = ['Hytte A1', 'Hytte A2', 'Hytte B1', 'Hytte B2', 'Hytte C1', 'Hytte C2', 'Hytte D1', 'Hytte D2'];
 
-	/**       Eksempel på et json format der skal bruges:
-	 *  {"Navn": "", "Koen": "M", "Hytte": "", "Tjans1": "", "Bus": "", "Slutrengoring": ""},
-	 */
-	let deltagere = [];
+	let k, i;
 
 	function opdaterHytter(hytteNavn) {
 		if (hytteNavn === "Hytte B1") {
@@ -103,6 +151,45 @@ $(document).ready(() => {
 			return HytteA2[0] < HytteA2[1];
 		}
 	}
+
+	/** Hytte fordeling */
+	$("#FordelHytter").click(() => {
+		const antalDrengeHytter = listeAfDrengeHytter.length;
+		for (i = 0; i < participants.length; i++) {
+			if (participants[i].Sex === "K") {
+				if (HyttePiger[0] < HyttePiger[1]) {
+					participants[i].Hytte = HyttePiger[2];
+					HyttePiger[0] += 1;
+				} else {
+					console.warn("Too many girls")
+				}
+			} else if (participants[i].Sex === "M") {
+				const randomizer = Math.floor(Math.random() * antalDrengeHytter);
+				const valgtHytte = listeAfDrengeHytter[randomizer];
+				if (tjekHytte(valgtHytte)) {
+					participants[i].Hytte = valgtHytte;
+					opdaterHytter(valgtHytte)
+				} else {
+					let hytteAssigned = false;
+					k = randomizer;
+					while (!hytteAssigned) {
+						k++;
+						if (k >= antalDrengeHytter) {
+							k = 0;
+						}
+						let hytteFound = listeAfDrengeHytter[k];
+						if (tjekHytte(hytteFound)) {
+							participants[i].Hytte = hytteFound;
+							opdaterHytter(hytteFound);
+							hytteAssigned = true;
+						} else {
+							hytteAssigned = false;
+						}
+					}
+				}
+			}
+		}
+	});
 
 	function opdaterTjanser(tjanseNavn, i, tjansNr) {
 		let fundenTjans = null;
@@ -177,11 +264,11 @@ $(document).ready(() => {
 			tjansDag = rengoringUdenforMandag[3];
 		}
 		if (tjansNr === 1) {
-			deltagere[i].Tjans1 = fundenTjans;
-			deltagere[i].Tjansdag = tjansDag;
+			participants[i].Tjans1 = fundenTjans;
+			participants[i].Tjansdag = tjansDag;
 		} else {
-			deltagere[i].Tjans2 = fundenTjans;
-			deltagere[i].Tjans2dag = tjansDag;
+			participants[i].Tjans2 = fundenTjans;
+			participants[i].Tjans2dag = tjansDag;
 		}
 		remainingTjanser -= 1;
 	}
@@ -224,78 +311,12 @@ $(document).ready(() => {
 		}
 	}
 
-	function opdaterSlutTjanser(tjanseNavn, i) {
-		let fundenSlutTjans = null;
-		if (tjanseNavn === "slutRengøringIndenfor") {
-			slutRengoringIndenfor[0]++;
-			fundenSlutTjans = slutRengoringIndenfor[2];
-		} else if (tjanseNavn === "slutRengøringUdenfor") {
-			slutRengoringUdenfor[0]++;
-			fundenSlutTjans = slutRengoringUdenfor[2];
-		} else if (tjanseNavn === "slutRengøringKøkken") {
-			slutRengoringKokken[0]++;
-			fundenSlutTjans = slutRengoringKokken[2];
-		}
-		deltagere[i].Slutrengoring = fundenSlutTjans;
-	}
-
-	function tjekSlutTjans(tjanseNavn) {
-		if (tjanseNavn === "slutRengøringIndenfor") {
-			return slutRengoringIndenfor[0] < slutRengoringIndenfor[1];
-		} else if (tjanseNavn === "slutRengøringUdenfor") {
-			return slutRengoringUdenfor[0] < slutRengoringUdenfor[1];
-		} else if (tjanseNavn === "slutRengøringKøkken") {
-			return slutRengoringKokken[0] < slutRengoringKokken[1];
-		}
-	}
-
-	let antalTjanser = listeAfTjanser.length;
-	let antalSlutTjanser = listeAfSlutrengoring.length;
-	let busNr = 1;
-	let k, i;
-
-	/** Hytte fordeling */
-	$("#FordelHytter").click(() => {
-		const antalDrengeHytter = listeAfDrengeHytter.length;
-		for (i = 0; i < deltagere.length; i++) {
-			if (deltagere[i].Koen === "K") {
-				if (HyttePiger[0] < HyttePiger[1]) {
-					deltagere[i].Hytte = HyttePiger[2];
-					HyttePiger[0] += 1;
-				} else {
-					console.warn("Too many girls")
-				}
-			} else if (deltagere[i].Koen === "M") {
-				const randomizer = Math.floor(Math.random() * antalDrengeHytter);
-				const valgtHytte = listeAfDrengeHytter[randomizer];
-				if (tjekHytte(valgtHytte)) {
-					deltagere[i].Hytte = valgtHytte;
-					opdaterHytter(valgtHytte)
-				} else {
-					let hytteAssigned = false;
-					k = randomizer;
-					while (!hytteAssigned) {
-						k++;
-						if (k >= antalDrengeHytter) {
-							k = 0;
-						}
-						let hytteFound = listeAfDrengeHytter[k];
-						if (tjekHytte(hytteFound)) {
-							deltagere[i].Hytte = hytteFound;
-							opdaterHytter(hytteFound);
-							hytteAssigned = true;
-						} else {
-							hytteAssigned = false;
-						}
-					}
-				}
-			}
-		}
-	});
-
 	/** Tjanse fordeling */
 	$("#FordelTjanser").click(() => {
-		for (i = 0; i < deltagere.length; i++) {
+		for (i = 0; i < participants.length; i++) {
+            if (remainingTjanser === 0) {
+				break;
+			}
 			const tjanseRandomizer = Math.floor(Math.random() * antalTjanser);
 			const valgtTjans = listeAfTjanser[tjanseRandomizer];
 			if (tjekTjans(valgtTjans)) {
@@ -318,10 +339,15 @@ $(document).ready(() => {
 				}
 			}
 		}
-	});
+    });
+    
 	/** Tjanse2 fordeling */
 	$("#FordelTjanserTo").click(() => {
-		for (i = 0; i < deltagere.length; i++) {
+		for (i = 0; i < participants.length; i++) {
+            if (remainingTjanser === 0) {
+				console.log("We had to break the loop with ", remainingTjanser, " tjanser remaining");
+				break;
+			}
 			const tjanseRandomizer = Math.floor(Math.random() * antalTjanser);
 			const valgtTjans = listeAfTjanser[tjanseRandomizer];
 			if (tjekTjans(valgtTjans)) {
@@ -343,28 +369,37 @@ $(document).ready(() => {
 					}
 				}
 			}
-			if (remainingTjanser === 0) {
-				break;
-			}
 		}
 	});
 
-	/** Fordeling i busserne */
-	$("#FordelBusserne").click(() => {
-		for (i = 0; i < deltagere.length; i++) {
-			if (busNr === 1) {
-				deltagere[i].Bus = Bus1;
-				busNr += 1;
-			} else {
-				deltagere[i].Bus = Bus2;
-				busNr = 1;
-			}
+	function opdaterSlutTjanser(tjanseNavn, i) {
+		let fundenSlutTjans = null;
+		if (tjanseNavn === "slutRengøringIndenfor") {
+			slutRengoringIndenfor[0]++;
+			fundenSlutTjans = slutRengoringIndenfor[2];
+		} else if (tjanseNavn === "slutRengøringUdenfor") {
+			slutRengoringUdenfor[0]++;
+			fundenSlutTjans = slutRengoringUdenfor[2];
+		} else if (tjanseNavn === "slutRengøringKøkken") {
+			slutRengoringKokken[0]++;
+			fundenSlutTjans = slutRengoringKokken[2];
 		}
-	});
+		participants[i].Slutrengoring = fundenSlutTjans;
+	}
+
+	function tjekSlutTjans(tjanseNavn) {
+		if (tjanseNavn === "slutRengøringIndenfor") {
+			return slutRengoringIndenfor[0] < slutRengoringIndenfor[1];
+		} else if (tjanseNavn === "slutRengøringUdenfor") {
+			return slutRengoringUdenfor[0] < slutRengoringUdenfor[1];
+		} else if (tjanseNavn === "slutRengøringKøkken") {
+			return slutRengoringKokken[0] < slutRengoringKokken[1];
+		}
+	}
 
 	/** Fordeling af slutrengøring */
 	$("#FordelSlutrengøring").click(() => {
-		for (i = 0; i < deltagere.length; i++) {
+		for (i = 0; i < participants.length; i++) {
 			const slutTjanseRandomizer = Math.floor(Math.random() * antalSlutTjanser);
 			const valgtSlutTjans = listeAfSlutrengoring[slutTjanseRandomizer];
 			if (tjekSlutTjans(valgtSlutTjans)) {
@@ -389,14 +424,27 @@ $(document).ready(() => {
 		}
 	});
 
+	/** Fordeling i busserne */
+	$("#FordelBusserne").click(() => {
+		for (i = 0; i < participants.length; i++) {
+			if (busNr === 1) {
+				participants[i].Bus = Bus1;
+				busNr += 1;
+			} else {
+				participants[i].Bus = Bus2;
+				busNr = 1;
+			}
+		}
+	});
+
 	$("#Log").click(() => {
-		console.log(deltagere);
+		console.log(participants);
 		console.log(remainingTjanser);
 		console.log(opvaskFrokostMandag);
 	});
 
 	$("#localStorage").click(() => {
-		sessionStorage.setItem("Deltagere", JSON.stringify(deltagere));
+		sessionStorage.setItem("Participants", JSON.stringify(participants));
 	});
 
 });
